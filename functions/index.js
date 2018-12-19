058756443b6986d288e97a3ff53a40a9eb1d8e55
Sheila Wallace <s286171@P0119104.aepsc.com>
2018-12-19 15:37:58 -0500

    SW - Updating the final response

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ const {dialogflow} = require('actions-on-google');
 const app = dialogflow({debug: true});
 
 app.intent('final_confirm', (conv, {address}) => {
-    conv.close(`Thank you, your power has been reported at ${address}.`);
+    conv.close(`Thank you, your power has been reported at ${address}. Thanks for using Pillar Voice!`);
 });
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
